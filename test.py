01bbc84f033361fc2112bcbccbd147f037998ce0
Mukesh Badgujar <32473209+mukeshbadgujar@users.noreply.github.com>
2020-05-11 04:16:19 +0530

    tut 1

---
+++ test.py
@@ -0,0 +1,33 @@
+print("Hello World")
+
+print(2+1)
+print(2-4)
+print(4/7)
+print(5//6)
+print(4+2*4)
+print((5+7)*2)
+print(2**5)
+print(10%2)
+
+print("MSB's")
+print('MSB"s')
+print('MSB\'s Computer')
+print(r"MSB's Location: C:\MSB\name")
+
+a = 2
+b = 3
+print(a*b)
+
+a, b = 2, 5
+print(a+b)
+
+a = 6
+b = 9
+a, b = b, a
+print(a,b)
+
+
+
+
+
+
