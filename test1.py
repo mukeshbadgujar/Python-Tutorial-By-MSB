1a63ca7ab020cf33eacb2992f15322bb4f9755f4
Mukesh Badgujar <32473209+mukeshbadgujar@users.noreply.github.com>
2020-05-11 04:44:14 +0530

    tut 2

---
+++ test1.py
@@ -0,0 +1,26 @@
+name = 'this Is My First Language Python'
+
+print(name)
+print(name[-1])
+print(name[1])
+
+print(name[0::2])
+
+print(name[0]+'H'+name[2:])
+
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.find("Python"))
+print(name[26:])
+
+print(name.replace("this","It"))
+
+print(type(name))
+
+print(name.isalnum())
+print(name.isalpha())
+print(name.endswith("Python"))
+print(name.count("i"))
+
+
